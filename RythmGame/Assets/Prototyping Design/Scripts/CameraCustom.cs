4b539c8a25d0fb0afdacaf61a842a1af8149f369
Jamesstahl98 <Jamesstahl556@gmail.com>
2023-04-12 11:38:53 +0200

    sceneChanges

--- RythmGame/Assets/Prototyping Design/Scripts/CameraCustom.cs
+++ RythmGame/Assets/Prototyping Design/Scripts/CameraCustom.cs
@@ -7,12 +7,6 @@ public class CameraCustom : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log(TransparencySortMode.Perspective);
     }
 }
