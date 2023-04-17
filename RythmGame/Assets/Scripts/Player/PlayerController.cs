dc737175b1687e489e77b4de1a29fcaf8ec81386
BuntenHaren <44321870+BuntenHaren@users.noreply.github.com>
2023-04-17 12:29:44 +0200

    Oh well, dash works as instructed
    Some meta files just went and deleted themselves apparently??? wtf...

--- RythmGame/Assets/Scripts/Player/PlayerController.cs
+++ RythmGame/Assets/Scripts/Player/PlayerController.cs
@@ -1,34 +1,48 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement Variables")]
     [SerializeField]
-    private float baseMovementSpeed;
+    private float baseMovementSpeed = 10;
+    [Header("Dash Variables")]
     [SerializeField]
-    private float baseDashCooldown;
+    private float baseDashCooldown = 2;
     [SerializeField]
-    private float baseDashForce;
+    private float baseDashDistance = 5;
+    [SerializeField] 
+    private float baseDashDuration = 0.1f;
 
-    private float currentDashForce;
+    //private dash variables
+    private float currentDashDistance;
+    private float currentDashDuration;
     private float currentDashCooldownAmount;
     private bool dashReady = true;
+    private Timer dashTimer;
+    
+    //private movement variables
     private float currentMovementSpeed;
     private Rigidbody rb;
     private Vector3 newMove;
     private Vector2 moveDir;
-    private Timer dashTimer;
+
+    private PHealth playerHealth;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        
-        //Set some dash variables
+        if(!TryGetComponent<PHealth>(out playerHealth))
+            playerHealth = null;
+            
+        //Set some current variables
         dashTimer = new Timer();
         dashTimer.TimerDone += () => dashReady = true;
-        currentDashForce = baseDashForce;
+        currentDashDistance = baseDashDistance;
+        currentDashDuration = baseDashDuration;
         currentDashCooldownAmount = baseDashCooldown;
-        
         currentMovementSpeed = baseMovementSpeed;
     }
 
@@ -63,11 +77,24 @@ public class PlayerController : MonoBehaviour
     {
         if(!dashReady)
             return;
-        
-        dashReady = false;
-        rb.AddForce(newMove * currentDashForce, ForceMode.Impulse);
+
+        Vector3 targetPosition = transform.position + new Vector3(moveDir.x, 0, moveDir.y) * currentDashDistance;
+        StartCoroutine(LerpPosition(targetPosition, currentDashDuration));
+        playerHealth.MakeInvurnerableForTime(currentDashDuration);
         dashTimer.StartTimer(currentDashCooldownAmount);
-        Debug.Log("Dashed");
+        dashReady = false;
     }
 
+    private IEnumerator LerpPosition(Vector3 targetPosition, float duration)
+    {
+        float time = 0;
+        Vector3 startPosition = transform.position;
+        while (time < duration)
+        {
+            transform.position = Vector3.Lerp(startPosition, targetPosition, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetPosition;
+    }
 }
