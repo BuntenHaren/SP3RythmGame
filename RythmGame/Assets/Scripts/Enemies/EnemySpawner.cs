1019db50f5bb321c1d15d67429573bad9eee0bfe
BuntenHaren <44321870+BuntenHaren@users.noreply.github.com>
2023-04-20 17:35:56 +0200

    EnemySpawner

---
+++ RythmGame/Assets/Scripts/Enemies/EnemySpawner.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject objectToSpawn;
+    [SerializeField]
+    private float timeBetweenSpawns;
+    [SerializeField]
+    private int totalAmountToSpawn;
+    [SerializeField]
+    private List<EnemyBehavior> enemiesToKillBefore;
+    [SerializeField]
+    private List<Transform> spawnPoints;
+
+    private int spawnsLeft;
+    private int currentSpawnPoint;
+    private Timer spawnTimer;
+
+    private void Start()
+    {
+        spawnsLeft = totalAmountToSpawn;
+        spawnTimer = new Timer();
+        spawnTimer.TimerDone += SpawnObject;
+        if(spawnPoints.Count == 0)
+            spawnPoints.Add(transform);
+    }
+
+    private void SpawnObject()
+    {
+        if(spawnsLeft <= 0)
+            return;
+        
+        Instantiate(objectToSpawn, spawnPoints[currentSpawnPoint]);
+        currentSpawnPoint++;
+        if(currentSpawnPoint == spawnPoints.Count)
+            currentSpawnPoint = 0;
+        spawnTimer.StartTimer(timeBetweenSpawns);
+        spawnsLeft--;
+    }
+
+    private void Update()
+    {
+        spawnTimer.UpdateTimer(Time.deltaTime);
+        bool startSpawn = true;
+        for(int i = 0; i < enemiesToKillBefore.Count; i++)
+        {
+            if(enemiesToKillBefore[i].enabled)
+                startSpawn = false;
+        }
+        if(startSpawn)
+            spawnTimer.StartTimer(timeBetweenSpawns);
+    }
+}
