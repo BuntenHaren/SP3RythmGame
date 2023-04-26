896196696421f6374e59fcd9a11575f195faef93
Jamesstahl98 <Jamesstahl556@gmail.com>
2023-04-26 19:13:45 +0200

    Healthbar beat

--- RythmGame/Assets/Scripts/UI/PlayerHealthBar.cs
+++ RythmGame/Assets/Scripts/UI/PlayerHealthBar.cs
@@ -6,14 +6,45 @@ using DG.Tweening;
 
 public class PlayerHealthBar : MonoBehaviour
 {
+<<<<<<< Updated upstream
+=======
+    [SerializeField]
+    private MusicEventPort eventPort;
+
+    [SerializeField] 
+    private Health playerHealth;
+>>>>>>> Stashed changes
     [SerializeField]
     public Slider slider;
     [SerializeField]
-    private float animationTime;
+    private float scaleUpTime;
+    [SerializeField]
+    private float scaleDownTime;
     [SerializeField]
     private Vector3 scaleTo;
 
+<<<<<<< Updated upstream
     public void SetMaxHealth(float health)
+=======
+    private void Start()
+    {
+        SetMaxHealth(playerHealth.MaxHealth);
+
+        eventPort.onBeat += HealthBarBeatAnimation;
+    }
+
+    private void OnEnable()
+    {
+        playerHealth.onChange += SetHealth;
+    }
+
+    private void OnDisable()
+    {
+        playerHealth.onChange -= SetHealth;
+    }
+
+    private void SetMaxHealth(float health)
+>>>>>>> Stashed changes
     {
         slider.maxValue = health;
         slider.value = health;
@@ -22,16 +53,13 @@ public class PlayerHealthBar : MonoBehaviour
     public void SetHealth(float health)
     {
         slider.value = health;
-        HealthBarAnimation();
     }
 
-    private void HealthBarAnimation()
+    private void HealthBarBeatAnimation()
     {
-        transform.DOScale(scaleTo, animationTime).SetEase(Ease.InOutBounce).OnComplete(ResetTransformAnimation);
-    }
-
-    private void ResetTransformAnimation()
-    {
-        transform.DOScale(new Vector3 (1f, 1f, 1f), animationTime).SetEase(Ease.InOutBounce);
+        transform.DOScale(scaleTo, scaleUpTime).SetEase(Ease.InOutBounce).OnComplete(() =>
+        {
+            transform.DOScale(new Vector3(1f, 1f, 1f), scaleDownTime).SetEase(Ease.InOutSine);
+        });
     }
 }
