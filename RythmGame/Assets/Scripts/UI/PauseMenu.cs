35ec002b660600386a9405bb733114104852b7c3
Jamesstahl98 <Jamesstahl556@gmail.com>
2023-04-26 18:09:21 +0200

    Pause Menu

---
+++ RythmGame/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    private bool optionsOpen = false;
+    private bool videoSettingsOpen = false;
+    private bool audioSettingsOpen = false;
+
+    [SerializeField]
+    private GameObject pauseMenuUI;
+    [SerializeField]
+    private GameObject mainPauseUI;
+    [SerializeField]
+    private GameObject optionsMenuUI;
+    [SerializeField]
+    private GameObject audioSettingsUI;
+    [SerializeField]
+    private GameObject videoSettingsUI;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(videoSettingsOpen || audioSettingsOpen)
+            {
+                BackToOptions();
+            }
+            else if(optionsOpen)
+            {
+                BackToPauseMenu();
+            }
+            else if(gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        mainPauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        mainPauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void Options()
+    {
+        optionsOpen = true;
+        mainPauseUI.SetActive(false);
+        optionsMenuUI.SetActive(true);
+    }
+
+    public void BackToPauseMenu()
+    {
+        optionsOpen = false;
+        optionsMenuUI.SetActive(false);
+        mainPauseUI.SetActive(true);
+    }
+
+    public void VideoSettings()
+    {
+        videoSettingsOpen = true;
+        optionsOpen = false;
+        optionsMenuUI.SetActive(false);
+        videoSettingsUI.SetActive(true);
+    }
+
+    public void AudioSettings()
+    {
+        audioSettingsOpen = true;
+        optionsOpen = false;
+        optionsMenuUI.SetActive(false);
+        audioSettingsUI.SetActive(true);
+    }
+
+    public void BackToOptions()
+    {
+        audioSettingsOpen = false;
+        videoSettingsOpen = false;
+        optionsOpen = true;
+        audioSettingsUI.SetActive(false);
+        videoSettingsUI.SetActive(false);
+        optionsMenuUI.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
