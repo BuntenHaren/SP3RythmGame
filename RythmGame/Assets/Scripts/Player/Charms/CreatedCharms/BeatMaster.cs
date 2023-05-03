7e189896cb8afd2866865b6bed2c678eef2cff7e
1Dicul <gighmail@gmail.com>
2023-05-03 11:52:01 +0200

    Charm scripts

---
+++ RythmGame/Assets/Scripts/Player/Charms/CreatedCharms/BeatMaster.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "RythmGame/Player/Beat Master")]
+public class BeatMaster : PassiveCharm
+{
+    private float JuiceLevelRatio = 0f;
+    private float BeatAccuracy = 0f;
+
+    private float BeatAccuracyMultiplier = 0.5f;
+    private float BaseMultiplier = 0.7f;
+    private float JuiceLevelMultiplier = 1f;
+
+    private float DistanceToLastBeat = 0f;
+    private float DistanceToNextBeat = 0f;
+    private float BeatWindow;
+
+    public override void Start()
+    {
+        base.Start();
+
+        // get beat window 
+        BeatWindow = playerStats.CurrentTimeForBeatWindow;
+    }
+
+    public override void OnPlayerAttackAction()
+    {
+        // calculate juice gauge fill ratio
+        JuiceLevelRatio = juiceCounter.CurrentJuice / juiceCounter.MaxJuice;
+
+        DistanceToLastBeat = beatPort.GetDistanceToLastBeat();
+        DistanceToNextBeat = beatPort.GetDistanceToNextBeat();
+
+        
+        // add flat damage if any beat is within beat window
+        if ((DistanceToLastBeat <= BeatWindow) || (DistanceToNextBeat <= BeatWindow))
+        {
+            BeatAccuracy = BeatAccuracyMultiplier;
+        }
+        else
+        {
+            BeatAccuracy = 0f;
+        }
+
+        // set damage multiplier
+        playerStats.AttackDamageMultiplier = BaseMultiplier + JuiceLevelMultiplier * JuiceLevelRatio + BeatAccuracy;
+        Debug.Log("Attack Damage Multiplier: " + playerStats.AttackDamageMultiplier);
+        Debug.Log("Attack Damage Multiplier: " + playerStats.AttackDamageMultiplier);
+    }
+}
