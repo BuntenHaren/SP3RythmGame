9a0f12b3552fb1d722634d64d1aa61df58daf8db
Jamesstahl98 <Jamesstahl556@gmail.com>
2023-05-04 12:44:41 +0200

    AttackOnBeat tutorial

---
+++ RythmGame/Assets/Scripts/Misc/AttackOnBeatTutorial.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class AttackOnBeatTutorial : MonoBehaviour, IDamageable
+{
+    [SerializeField]
+    private MusicEventPort musicEventPort;
+    [SerializeField]
+    private MoveObject[] moveObjects;
+    [SerializeField]
+    private RotateObject[] rotateObjects;
+    [SerializeField]
+    private GameObject wallCollider;
+
+    //Beat Scale
+    [SerializeField]
+    private Vector3 scaleTo;
+    [SerializeField]
+    private float scaleUpTime;
+    [SerializeField]
+    private float scaleDownTime;
+    [SerializeField]
+    private Vector3 originalScale;
+
+    private SpriteRenderer sr;
+
+    void Start()
+    {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+        musicEventPort.onBeat += OnBeat;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        sr.DOFade(0f, 0.7f).OnComplete(() =>
+        {
+            wallCollider.SetActive(false);
+            gameObject.SetActive(false);
+        });
+        for (int i = 0; i < moveObjects.Length; i++)
+        {
+            moveObjects[i].StartMove();
+        }
+
+        for (int j = 0; j < rotateObjects.Length; j++)
+        {
+            rotateObjects[j].StartRotation();
+        }
+    }
+
+    public void TakeDamageOnBeat(float damage)
+    {
+        sr.DOFade(0f, 0.7f).OnComplete(() =>
+        {
+            wallCollider.SetActive(false);
+            gameObject.SetActive(false);
+        });
+        for (int i = 0; i < moveObjects.Length; i++)
+        {
+            moveObjects[i].StartMove();
+        }
+
+        for (int j = 0; j < rotateObjects.Length; j++)
+        {
+            rotateObjects[j].StartRotation();
+        }
+    }
+
+    public void HealDamage(float damage)
+    {
+
+    }
+
+    private void OnBeat()
+    {
+        transform.DOScale(scaleTo, scaleUpTime).SetEase(Ease.InOutSine).OnComplete(() =>
+        {
+            transform.DOScale(originalScale, scaleDownTime).SetEase(Ease.InOutSine);
+        });
+    }
+
+}
---
+++ RythmGame/Assets/Scripts/Misc/MoveObject.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MoveObject : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 targetPos;
+    [SerializeField]
+    private float duration;
+
+    public void StartMove()
+    {
+        transform.DOMove(targetPos, duration);
+    }
+}
