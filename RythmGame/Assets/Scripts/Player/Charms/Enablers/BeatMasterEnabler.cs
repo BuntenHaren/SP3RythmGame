0d2a650c9e33efcb83a1282cb0b165c7af9e4e69
1Dicul <gighmail@gmail.com>
2023-05-15 15:04:11 +0200

    charms & scenes

--- RythmGame/Assets/Scripts/Player/Charms/Enablers/BeatMasterEnabler.cs
+++ RythmGame/Assets/Scripts/Player/Charms/Enablers/BeatMasterEnabler.cs
@@ -19,5 +19,11 @@ public class BeatMasterEnabler : MonoBehaviour
         }
     }
 
-    
+    void OnTriggerExit(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            textBox.SetActive(false);
+        }
+    }
 }
